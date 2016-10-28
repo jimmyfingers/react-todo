dbccc05834975e89783a4b59f4ae00059a5442ea
jimmyfingers <Home@Jamess-MacBook-Pro.local>
2016-10-28 14:06:57 +0100

    Refactor todo list todo and addTodo to work with redux

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
-var $ = require('jquery');
+var $ = require('jQuery');
 
 var {Todo} = require('Todo');
 
@@ -17,15 +17,17 @@ describe('Todo', () => {
       text: 'Write todo.test.jsx test',
       completed: true
     };
+
     var spy = expect.createSpy();
     var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>);
     var $el = $(ReactDOM.findDOMNode(todo));
-
     TestUtils.Simulate.click($el[0]);
 
     expect(spy).toHaveBeenCalledWith({
       type: 'TOGGLE_TODO',
       id: todoData.id
     });
+
   });
+
 });
